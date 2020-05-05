68fe667b00c4f41cd04754a91d5aa36f4d08a80b
Pierre-Julien VILLOUD <31385588+pjvilloud@users.noreply.github.com>
2020-05-05 17:08:33 +0200

    Update and rename SpringApplication.java to Application.java

---
+++ src/main/java/com/ipiecoles/java/java240/Application.java
@@ -0,0 +1,59 @@
+package com.ipiecoles.java.java240;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.net.ssl.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+@SpringBootApplication
+//équivalent à @Configuration @EnableAutoConfiguration @ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${bitcoinService.forceRefresh}")
+    private Boolean forceRefresh;
+
+    @Bean(name = "bitcoinServiceCacheProperty")
+    public BitcoinService bitcoinServiceCacheProperty(){
+        System.out.println("Instantiation du bean BitcoinService sans cache par Spring");
+        BitcoinService bitcoinService = new BitcoinService();
+        bitcoinService.setForceRefresh(forceRefresh);
+        return bitcoinService;
+    }
+
+    @Bean(name = "disableSSL")
+    public Boolean disableSSLValidation() throws Exception {
+        final SSLContext sslContext = SSLContext.getInstance("TLS");
+
+        sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, null);
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+}
